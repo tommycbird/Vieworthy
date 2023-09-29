c6050f349cb9ba859550c04f40b5e28d2994c896
Henry Webb <64293535+webbhenry1@users.noreply.github.com>
2023-09-29 12:17:32 -0400

    YouTube API now gets the Comments

--- src/js/script.js
+++ src/js/script.js
@@ -2,38 +2,50 @@
 
 //CODE TO FETCH VIDEO DATA FROM YOUTUBE API
 function fetchVideoData(url) {
-    // Extract the video ID from the URL
     const videoID = extractVideoID(url);
-
     if (!videoID) {
         console.error("Invalid YouTube URL");
         return;
     }
+
     const API_KEY = "AIzaSyCe3jBxqhacGmezpofd5olN3Cv5Qmjy_mE";
-    const apiUrl = `https://www.googleapis.com/youtube/v3/videos?id=${videoID}&key=${API_KEY}&part=snippet,statistics`;
+    const videoApiUrl = `https://www.googleapis.com/youtube/v3/videos?id=${videoID}&key=${API_KEY}&part=snippet,statistics`;
+
+    // Constructing the API URL for fetching comments
+    const commentApiUrl = `https://www.googleapis.com/youtube/v3/commentThreads?part=snippet&videoId=${videoID}&key=${API_KEY}&maxResults=100`;
+
     return new Promise((resolve, reject) => {
-        fetch(apiUrl)
+        fetch(videoApiUrl)
             .then(response => response.json())
             .then(data => {
                 if (data.items && data.items.length > 0) {
                     const snippet = data.items[0].snippet;
                     const statistics = data.items[0].statistics;
-                    
-                    // Access various data points
+
                     const details = {
                         title: snippet.title,
                         description: snippet.description,
                         likes: statistics.likeCount,
+                        comments: [] // initializing comments as an empty array
                     };
-                    // Update the UI elements individually
-                    document.getElementById('title').innerText = details.title;
-                    document.getElementById('description').innerText = details.description;
-                    document.getElementById('likes').innerText = 'Likes: ' + details.likes;
 
-                    // Display the popup once the data is fetched
-                    displayPopup();
+                    // Fetch comments
+                    fetch(commentApiUrl)
+                        .then(commentResponse => commentResponse.json())
+                        .then(commentData => {
+                            if (commentData.items) {
+                                commentData.items.forEach(item => {
+                                    const comment = item.snippet.topLevelComment.snippet.textDisplay;
+                                    details.comments.push(comment);
+                                });
+                            }
 
-                    resolve(details);
+                            resolve(details);
+                        })
+                        .catch(commentError => {
+                            console.error("Failed to fetch comments:", commentError);
+                            resolve(details); // Resolve the promise even if fetching comments fails
+                        });
                 } else {
                     console.log('Video not found.');
                     reject('Video not found.');
@@ -45,6 +57,7 @@ function fetchVideoData(url) {
             });
     });
 }
+
 
 
 
@@ -129,24 +142,28 @@ function compute() {
         .then(details => {
             console.log("HERE ARE THE DETAILS", details);
 
-            // // Fetch the additional data using Selenium after fetching the video data
-            // getTranscription(url).then(transcription => {
-            //     console.log("HERE IS THE TRANSCRIPTION", transcription);
-            //     details.transcription = transcription; // add transcription to details object
-                
-            //     getDislikes(url).then(dislikes => {
-            //         console.log("HERE ARE THE DISLIKES", dislikes);
-            //         details.dislikes = dislikes; // add dislikes to details object
+            document.getElementById('title').innerText = details.title;
+            document.getElementById('description').innerText = details.description;
+            document.getElementById('likes').innerText = details.likes;
+            
+            let commentsDiv = document.getElementById('comments');
+            if (details.comments && details.comments.length > 0) {
+                let commentList = '<ul style="max-height:200px; overflow-y:scroll;">';
+                details.comments.forEach(comment => {
+                    commentList += '<li>' + comment + '</li>';
+                });
+                commentList += '</ul>';
+                commentsDiv.innerHTML = commentList;
+            }
 
-            //         console.log("HERE ARE THE FINAL DETAILS", details);
-            //         // Here, you have a details object containing all the data you fetched
-            //     });
-            // });
+            // Display the popup
+            displayPopup();
         })
         .catch(error => {
             console.error('Error fetching video data:', error);
         });
 }
+
 
 
 
