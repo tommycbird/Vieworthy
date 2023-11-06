cdd338d40d1d20dba1e7a78a2289741dfe29a01e
Henry Webb <64293535+webbhenry1@users.noreply.github.com>
2023-11-06 15:22:05 -0500

    Selenium why

--- src/js/get_transcript.js
+++ src/js/get_transcript.js
@@ -1,53 +1,106 @@
-const {Builder, Browser, By, Key, until, Capabilities} = require('selenium-webdriver');
-const chrome = require('selenium-webdriver/chrome');
+const {Builder, Browser, By, until} = require('selenium-webdriver');
+const chrome = require('selenium-webdriver/chrome'); 
+
+// ... rest of your unchanged code ...
 
 async function getTranscript(link) {
-    let transcript = [];
-    let chromeOptions = new chrome.Options();
-    chromeOptions.addArguments('--headless');
-    chromeOptions.addArguments('--no-sandbox');
+  let options = new chrome.Options();
+  options.addArguments('--headless');
+  // ... rest of your unchanged code ...
+}
+
+// ...
+
+function formatTranscript(istring) {
+  const parts = istring.split(' ');
+
+  let minutes = '0';
+  let seconds = '00';
+
+  if (parts[1] === 'second' || parts[1] === 'seconds') {
+    seconds = parts[0];
+    parts.splice(0,2);
+    if(parseInt(seconds) < 10 ) {
+      seconds = '0' + seconds;
+    }
+  } 
+  else if (parts[3] === 'second' || parts[3] === 'seconds') {
+    seconds = parts[2];
+    parts.splice(2,2);
+    if(parseInt(seconds) < 10 ) {
+      seconds = '0' + seconds;
+    }
+  }
+  if (parts[1] === 'minute' || parts[1] === 'minutes' || parts[1] === 'minute,' || parts[1] === 'minutes,') {
+    minutes = parts[0];
+    parts.splice(0,2);
+  } 
+
+  const ostring = minutes +':'+seconds +'; '+ parts.join(' ');
+  return ostring;
+}
+
+async function scrollUntilElement(driver, xpath) {
+  for (let i = 0; i < 10; i++) {
+    try {
+      driver.findElement(By.xpath(xpath));
+      break;
+    }
+    catch (error) {
+      driver.executeScript('window.scrollBy(0, 500);');
+      await new Promise(resolve => setTimeout(resolve, 200));
+    }
+  }
+
+}
+
+async function getTranscript(link) {
+    // Set up Chrome in headless mode
+    let options = new chrome.Options();
+    options.addArguments('--headless');
+    options.addArguments('--no-sandbox'); // NOTE: Use this flag if you understand the security implications.
+    options.addArguments('--disable-dev-shm-usage');
 
     let driver = await new Builder()
-        .forBrowser(Browser.CHROME)
-        .setChromeOptions(chromeOptions)
-        .build();
+    .forBrowser(Browser.CHROME)
+    .setChromeOptions(options)
+    .build();
 
-    try {
-        await driver.get(link);
+    let transcript = []; //storage for transcript
 
-        // Use a dynamic wait to ensure the page is loaded
-        await driver.wait(() => driver.executeScript('return document.readyState').then(state => state === 'complete'));
+  await driver.get(link); 
+  try{
+    //wait for description container to load
+    await driver.wait(until.elementLocated(By.xpath('//*[@id="expand"]')), 10000);
+    await new Promise(resolve => setTimeout(resolve, 1000));
 
-        // xpath click on 3 dots
-        await driver.findElement(By.xpath('//*[@id="button-shape"]/button')).click();
-        await driver.sleep(100);
+    //xpath click on description container
+    await driver.findElement(By.xpath('//*[@id="expand"]')).click();
 
-        // click on "show transcript"
-        await driver.findElement(By.xpath('//*[@id="items"]/ytd-menu-service-item-renderer[2]')).click();
-        await driver.sleep(500);
+    // scroll till 'show transcript' visible
+    await scrollUntilElement(driver, '//*[@id="primary-button"]/ytd-button-renderer/yt-button-shape/button');
 
-        // click "show transcipt" to load the actual transcript
-        driver.executeScript('window.scrollBy(0, 200);');
-        await driver.findElement(By.xpath('//*[@id="primary-button"]/ytd-button-renderer/yt-button-shape/button')).click();
-        await driver.sleep(500);
+    //click "show transcipt" to load the actual transcript
+    await driver.findElement(By.xpath('//*[@id="primary-button"]/ytd-button-renderer/yt-button-shape/button')).click();
+    await new Promise(resolve => setTimeout(resolve, 1000));
 
-        // get all elements with transcript text
-        let transcriptElements = await driver.findElements(By.className('segment style-scope ytd-transcript-segment-renderer'));
+    //get all elements with transcript text
+    let transcriptElements = await driver.findElements(By.className('segment style-scope ytd-transcript-segment-renderer'));
 
-        // get the text of each element
-        for(let element of transcriptElements) {
-            transcript.push(await element.getAttribute('aria-label'));
-        }
-
-    } catch (error) {
-        console.error("Error occurred: ", error);
-    } finally {
-        await driver.quit();
+    // get the text of each element
+    for(let i = 0; i < transcriptElements.length; i++){
+      let line = formatTranscript(await transcriptElements[i].getAttribute('aria-label'));
+      transcript.push(line);
     }
+  } 
+  catch (error) {
+    //quit if error
+    await driver.quit();
+    console.error('error in fetching link', error.message);
+  }
     
     await driver.quit();
     return transcript;
 };
-
 
 module.exports = getTranscript;
