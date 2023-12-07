de38e9e092ef716ff65159a1d04f4ecc32300f7f
Jimmy Zhen <jzhen03@users.noreply.github.com>
2023-12-07 18:50:51 +0000

    Automated JavaScript formatting changes

--- src/js/content.js
+++ src/js/content.js
@@ -1,147 +1,146 @@
-var s = document.createElement('script');
-s.src = chrome.runtime.getURL('src/js/script.js');
-s.onload = function() {
-    this.remove();
+var s = document.createElement("script");
+s.src = chrome.runtime.getURL("src/js/script.js");
+s.onload = function () {
+  this.remove();
 };
 (document.head || document.documentElement).appendChild(s);
 
 function injectCSS() {
-    const link = document.createElement('link');
-    link.href = chrome.runtime.getURL('src/css/extension.css');
-    link.type = 'text/css';
-    link.rel = 'stylesheet';
-    document.head.appendChild(link);
+  const link = document.createElement("link");
+  link.href = chrome.runtime.getURL("src/css/extension.css");
+  link.type = "text/css";
+  link.rel = "stylesheet";
+  document.head.appendChild(link);
 }
 
 function killPopup() {
-    console.log("Killing Popup...");
-    var popup = document.getElementById('popup');
-    // Check if the element actually exists
-    if (popup!=null) {
-        popup.remove();
-        console.log('Popup removed.');
-    } else {
-        // If the element is null, log an error message
-        console.log('Error: Tried to remove a popup that does not exist.');
-    }
+  console.log("Killing Popup...");
+  var popup = document.getElementById("popup");
+  // Check if the element actually exists
+  if (popup != null) {
+    popup.remove();
+    console.log("Popup removed.");
+  } else {
+    // If the element is null, log an error message
+    console.log("Error: Tried to remove a popup that does not exist.");
+  }
 }
 
 function insertPopup(key, sentence, entryID) {
-    console.log("Rendering Popup...");
-    // Fetch the content of popup.html
-    fetch(chrome.runtime.getURL('../popup.html'))
-        .then(response => response.text())
-        .then(data => {
-        // Create a new div element
-        const popup = document.createElement('div');
-        //popup.className = 'popup-container';
+  console.log("Rendering Popup...");
+  // Fetch the content of popup.html
+  fetch(chrome.runtime.getURL("../popup.html"))
+    .then((response) => response.text())
+    .then((data) => {
+      // Create a new div element
+      const popup = document.createElement("div");
+      //popup.className = 'popup-container';
 
-        // Set the innerHTML of the div to the fetched data
-        popup.innerHTML = data;
+      // Set the innerHTML of the div to the fetched data
+      popup.innerHTML = data;
 
-        // Append the popup to the body
-        document.body.appendChild(popup);
+      // Append the popup to the body
+      document.body.appendChild(popup);
 
-        popup.id = 'popup';
+      popup.id = "popup";
 
-        // Make popup visible and on the forefront
-        popup.style.zIndex = 2147483647;
-        popup.style.width = '100vw';
-        popup.style.height = '100vh';
-        popup.style.position = 'fixed'; // So that it stays in the viewport
-        popup.style.backdropFilter = 'blur(10px)';
-        popup.style.display = 'flex';
-        popup.style.justifyContent = 'center';
-        popup.style.alignItems = 'center';
+      // Make popup visible and on the forefront
+      popup.style.zIndex = 2147483647;
+      popup.style.width = "100vw";
+      popup.style.height = "100vh";
+      popup.style.position = "fixed"; // So that it stays in the viewport
+      popup.style.backdropFilter = "blur(10px)";
+      popup.style.display = "flex";
+      popup.style.justifyContent = "center";
+      popup.style.alignItems = "center";
 
-        // Add event listeners to the close button and toggle button
-        popup.querySelector('.close-button').addEventListener('click', killPopup);
-
+      // Add event listeners to the close button and toggle button
+      popup.querySelector(".close-button").addEventListener("click", killPopup);
     })
-        .catch(error => {
-        // Handle any errors that occurred during fetch
-        console.error('Error loading the popup:', error);
+    .catch((error) => {
+      // Handle any errors that occurred during fetch
+      console.error("Error loading the popup:", error);
     });
 }
-
 
 function injectButton() {
   injectCSS();
 
-  if (window.location.hostname === 'www.youtube.com') {
-      const timeElements = document.querySelectorAll('span[id="text"].style-scope.ytd-thumbnail-overlay-time-status-renderer');
+  if (window.location.hostname === "www.youtube.com") {
+    const timeElements = document.querySelectorAll(
+      'span[id="text"].style-scope.ytd-thumbnail-overlay-time-status-renderer',
+    );
 
-      //console.log(`Found ${timeElements.length} time elements.`);
+    //console.log(`Found ${timeElements.length} time elements.`);
 
-      timeElements.forEach(timeElement => {
-          let totalMinutes = 0;
+    timeElements.forEach((timeElement) => {
+      let totalMinutes = 0;
 
-          // Extract the video duration
-          const timeParts = timeElement.textContent.trim().split(':').map(Number);
+      // Extract the video duration
+      const timeParts = timeElement.textContent.trim().split(":").map(Number);
 
-          if (timeParts.length === 2) {
-              totalMinutes = timeParts[0] + timeParts[1] / 60;
-          } else if (timeParts.length === 3) {
-              totalMinutes = timeParts[0] * 60 + timeParts[1] + timeParts[2] / 60;
-          }
+      if (timeParts.length === 2) {
+        totalMinutes = timeParts[0] + timeParts[1] / 60;
+      } else if (timeParts.length === 3) {
+        totalMinutes = timeParts[0] * 60 + timeParts[1] + timeParts[2] / 60;
+      }
 
-          // If the video duration is less than 5 minutes, skip to the next iteration
-          if (totalMinutes < 5 || totalMinutes > 15) return;
+      // If the video duration is less than 5 minutes, skip to the next iteration
+      if (totalMinutes < 5 || totalMinutes > 15) return;
 
-          //Move upwards to find the nearest parent with id="thumbnail" to get video link
-          let thumbnail = timeElement.parentElement;
-          while (thumbnail && thumbnail.id !== "thumbnail") {
-            thumbnail = thumbnail.parentElement;
-          }
-          
-          
+      //Move upwards to find the nearest parent with id="thumbnail" to get video link
+      let thumbnail = timeElement.parentElement;
+      while (thumbnail && thumbnail.id !== "thumbnail") {
+        thumbnail = thumbnail.parentElement;
+      }
 
-          // Move upwards to find the nearest parent with id="content"
-          let contentParent = timeElement.parentElement;
-          while (contentParent && contentParent.id !== "content") {
-              contentParent = contentParent.parentElement;
-          }
+      // Move upwards to find the nearest parent with id="content"
+      let contentParent = timeElement.parentElement;
+      while (contentParent && contentParent.id !== "content") {
+        contentParent = contentParent.parentElement;
+      }
 
-          if (contentParent && !contentParent.querySelector(':scope > .assess-button')) {              
-              // Create the button
-              const sumButton = document.createElement('button');
-              sumButton.innerText = 'Assess';
-              sumButton.className = 'assess-button';
+      if (
+        contentParent &&
+        !contentParent.querySelector(":scope > .assess-button")
+      ) {
+        // Create the button
+        const sumButton = document.createElement("button");
+        sumButton.innerText = "Assess";
+        sumButton.className = "assess-button";
 
-              // Button event listener
-              sumButton.addEventListener('click', function(event) {
-                console.log('Summarize button clicked');
-                // Call insertPopupContainer when the button is clicked
-                insertPopup();
-                compute(thumbnail.href);
-              });
+        // Button event listener
+        sumButton.addEventListener("click", function (event) {
+          console.log("Summarize button clicked");
+          // Call insertPopupContainer when the button is clicked
+          insertPopup();
+          compute(thumbnail.href);
+        });
 
-              // Create the logo element
-              const img = document.createElement('img');
-              img.src = chrome.runtime.getURL('src/img/favicon.png');
-              img.width = 24;
-              img.height = 24;
-              img.style.marginRight = '10px'; 
+        // Create the logo element
+        const img = document.createElement("img");
+        img.src = chrome.runtime.getURL("src/img/favicon.png");
+        img.width = 24;
+        img.height = 24;
+        img.style.marginRight = "10px";
 
-              // Append the image to the button before the text
-              sumButton.prepend(img);
+        // Append the image to the button before the text
+        sumButton.prepend(img);
 
-              // Make meta relative
-              if (getComputedStyle(contentParent).position === 'static') {
-                  contentParent.style.position = 'relative';
-              }
+        // Make meta relative
+        if (getComputedStyle(contentParent).position === "static") {
+          contentParent.style.position = "relative";
+        }
 
-              
-              // Append the button to the content parent
-              contentParent.appendChild(sumButton);
-          }
-      });
+        // Append the button to the content parent
+        contentParent.appendChild(sumButton);
+      }
+    });
   }
 }
 
 // Run the function to inject the summarize button
 injectButton();
-
 
 // Since YouTube uses a lot of AJAX, run the function every time the DOM changes
 const observer = new MutationObserver(injectButton);
--- src/js/get_transcript.js
+++ src/js/get_transcript.js
@@ -1,39 +1,43 @@
-const {Builder, Browser, By, until} = require('selenium-webdriver');
-const chromeDriver = require('selenium-webdriver/chrome'); 
+const { Builder, Browser, By, until } = require("selenium-webdriver");
+const chromeDriver = require("selenium-webdriver/chrome");
 
 function formatTranscript(istring) {
-  const parts = istring.split(' ');
+  const parts = istring.split(" ");
 
-  let minutes = '0';
-  let seconds = '00';
+  let minutes = "0";
+  let seconds = "00";
 
-  if (parts[1] === 'second' || parts[1] === 'seconds') {
+  if (parts[1] === "second" || parts[1] === "seconds") {
     seconds = parts[0];
-    parts.splice(0,2);
-    if(parseInt(seconds) < 10 ) {
-      seconds = '0' + seconds;
+    parts.splice(0, 2);
+    if (parseInt(seconds) < 10) {
+      seconds = "0" + seconds;
     }
-  } 
-  else if (parts[3] === 'second' || parts[3] === 'seconds') {
+  } else if (parts[3] === "second" || parts[3] === "seconds") {
     seconds = parts[2];
-    parts.splice(2,2);
-    if(parseInt(seconds) < 10 ) {
-      seconds = '0' + seconds;
+    parts.splice(2, 2);
+    if (parseInt(seconds) < 10) {
+      seconds = "0" + seconds;
     }
   }
-  if (parts[1] === 'minute' || parts[1] === 'minutes' || parts[1] === 'minute,' || parts[1] === 'minutes,') {
+  if (
+    parts[1] === "minute" ||
+    parts[1] === "minutes" ||
+    parts[1] === "minute," ||
+    parts[1] === "minutes,"
+  ) {
     minutes = parts[0];
-    parts.splice(0,2);
-  } 
+    parts.splice(0, 2);
+  }
 
-  const ostring = minutes +':'+seconds +'; '+ parts.join(' ');
+  const ostring = minutes + ":" + seconds + "; " + parts.join(" ");
   return ostring;
 }
 
 async function scrollUntilElement(driver, xpath) {
   // Scroll to the top of the page first.
-  await driver.executeScript('window.scrollTo(0, 0);');
-  await new Promise(resolve => setTimeout(resolve, 3000)); 
+  await driver.executeScript("window.scrollTo(0, 0);");
+  await new Promise((resolve) => setTimeout(resolve, 3000));
 
   let element = null;
   for (let i = 0; i < 50; i++) {
@@ -42,12 +46,12 @@ async function scrollUntilElement(driver, xpath) {
       if (element) {
         let isDisplayed = await element.isDisplayed();
         if (isDisplayed) {
-          break; 
+          break;
         }
       }
     } catch (error) {
-      await driver.executeScript('window.scrollBy(0, 500);'); 
-      await new Promise(resolve => setTimeout(resolve, 3000)); 
+      await driver.executeScript("window.scrollBy(0, 500);");
+      await new Promise((resolve) => setTimeout(resolve, 3000));
     }
   }
 
@@ -58,70 +62,79 @@ async function scrollUntilElement(driver, xpath) {
   return element;
 }
 
-
 async function getTranscript(link) {
   let transcript = []; //storage for transcript
   let chromeOptions = new chromeDriver.Options();
-  chromeOptions.addArguments('--headless=new');
-  chromeOptions.addArguments('--no-sandbox');
-  chromeOptions.addArguments('--disable-dev-shm-usage');
+  chromeOptions.addArguments("--headless=new");
+  chromeOptions.addArguments("--no-sandbox");
+  chromeOptions.addArguments("--disable-dev-shm-usage");
 
   let driver = await new Builder()
     .forBrowser(Browser.CHROME)
     .setChromeOptions(chromeOptions)
     .build();
-  
-  try{
-    console.log('Starting getTranscript function');
-    console.log('Navigating to link:', link);
+
+  try {
+    console.log("Starting getTranscript function");
+    console.log("Navigating to link:", link);
     await driver.get(link);
-    console.log('Page loaded');
+    console.log("Page loaded");
     // updateStatus("Page Loaded");
 
-    console.log('Waiting for description container...');
-    await driver.wait(until.elementLocated(By.xpath('//*[@id="expand"]')), 30000);
-    console.log('Description container loaded');
+    console.log("Waiting for description container...");
+    await driver.wait(
+      until.elementLocated(By.xpath('//*[@id="expand"]')),
+      30000,
+    );
+    console.log("Description container loaded");
 
     // updateStatus("Navigating to Description....");
-    console.log('Clicking on description container...');
+    console.log("Clicking on description container...");
     await driver.findElement(By.xpath('//*[@id="expand"]')).click();
-    console.log('Clicked description container');
+    console.log("Clicked description container");
 
-
-    console.log('Waiting after click...');
-    await new Promise(resolve => setTimeout(resolve, 3000));
+    console.log("Waiting after click...");
+    await new Promise((resolve) => setTimeout(resolve, 3000));
 
     // updateStatus("Scrolling to transcript...");
-    await scrollUntilElement(driver, '//*[@id="primary-button"]/ytd-button-renderer/yt-button-shape/button');
-    console.log('Scrolled to transcript');
+    await scrollUntilElement(
+      driver,
+      '//*[@id="primary-button"]/ytd-button-renderer/yt-button-shape/button',
+    );
+    console.log("Scrolled to transcript");
 
     console.log('Clicking "show transcript" button...');
-    let button = await driver.findElement(By.xpath('//*[@id="primary-button"]/ytd-button-renderer/yt-button-shape/button'));
+    let button = await driver.findElement(
+      By.xpath(
+        '//*[@id="primary-button"]/ytd-button-renderer/yt-button-shape/button',
+      ),
+    );
     await driver.executeScript("arguments[0].click();", button);
     console.log('Clicked "show transcript" button');
     // updateStatus("Loading Transcript....");
 
+    console.log("Waiting after click...");
+    await new Promise((resolve) => setTimeout(resolve, 3000));
 
-    console.log('Waiting after click...');
-    await new Promise(resolve => setTimeout(resolve, 3000));
-
-    console.log('Fetching transcript lines...');
+    console.log("Fetching transcript lines...");
     // updateStatus("Reading in Transcript....");
-    let transcriptElements = await driver.findElements(By.className('segment style-scope ytd-transcript-segment-renderer'));
-    for(let i = 0; i < transcriptElements.length; i++){
-      let line = formatTranscript(await transcriptElements[i].getAttribute('aria-label'));
+    let transcriptElements = await driver.findElements(
+      By.className("segment style-scope ytd-transcript-segment-renderer"),
+    );
+    for (let i = 0; i < transcriptElements.length; i++) {
+      let line = formatTranscript(
+        await transcriptElements[i].getAttribute("aria-label"),
+      );
       transcript.push(line);
     }
-    console.log('Transcript lines fetched');
-  } 
-  catch (error) {
-    console.error('Error occurred while fetching the transcript:', error);
-  }
-  finally {
-    console.log('Quitting driver');
+    console.log("Transcript lines fetched");
+  } catch (error) {
+    console.error("Error occurred while fetching the transcript:", error);
+  } finally {
+    console.log("Quitting driver");
     await driver.quit();
   }
-    
+
   return transcript;
 }
 
